4d16349314110b12a0f4f318a4bb5479053c75ff
shohame <perry.shoham@gmail.com>
2023-10-04 21:38:30 +0300

    Done writing Brick and Ball classes.

--- code/break.py
+++ code/break.py
@@ -1,17 +1,28 @@
-
+import pygame
 from object import Object
+from globals import GameColors, GameDimensions
+
 class Brick(Object):
-    def __init__(self, xy, wh, color, type):
+    def __init__(self, xy, color, type):
+        wh = GameDimensions.BRICK_WH
         super().__init__(xy, wh, color, type)
 
+    def draw(self, screen):
+        x, y = self._xy
+        w, h = self._wh
+        color = self._color
+        pygame.draw.rect(screen, color, (x-w/2, y-h/2, w, h))
 
 
 
 if __name__=="__main__":
-    brick = Brick((400, 200), (50, 20), (255, 0, 0), "normal")
-    print(brick.get_location_xy_wh())
-    print(brick.get_type())
-    # Print the name of the class:
+    brick = Brick((400, 200), wh, (255, 0, 0), "normal")
+    print(f'brick.get_xy() = {brick.get_xy()}')
+    print(f'brick.get_wh() = {brick.get_wh()}')
+    print(f'brick.get_type() = {brick.get_type()}')
+    print(f'brick.get_color() = {brick.get_color()}')
+    print(f'brick.get_v_xy() = {brick.get_v_xy()}')
+     # Print the name of the class:
     print(brick.__class__.__name__)
     # Print the name of the base class:
     print(brick.__class__.__base__.__name__)
--- code/moving_object.py
+++ code/moving_object.py
@@ -1,10 +1,9 @@
 from object import Object
 
-class Brick(Object):
+class MovingObject(Object):
     def __init__(self, xy, wh, color, type, v_xy=(0, 0)):
         super().__init__(xy, wh, color, type)
         self._v_xy = v_xy
-
 
     def get_v_xy(self):
         return self._v_xy
@@ -16,3 +15,37 @@ class Brick(Object):
         x, y = self._xy
         vx, vy = self._v_xy
         self._xy = (x + vx * dt, y + vy * dt)
+
+    def bounce(self, axis):
+        vx, vy = self._v_xy
+        if axis == 'x':
+            self._v_xy = (-vx, vy)
+        elif axis == 'y':
+            self._v_xy = (vx, -vy)
+        else:
+            raise ValueError(f'axis must be x or y, not {axis}')
+
+    def hit_wall(self):
+        x, y = self._xy
+        vx, vy = self._v_xy
+        w, h = self._wh
+        return x - w/2 <= 0 or x + w/2 >= 100 # TODO: change to SCREEN_WIDTH
+
+
+if __name__=="__main__":
+    # testing MovingObject:
+    brick = MovingObject((100, 200), (50, 20), (255, 0, 0), "normal", (1, 2))
+    print(f'brick.get_xy() = {brick.get_xy()}')
+    print(f'brick.get_wh() = {brick.get_wh()}')
+    print(f'brick.get_type() = {brick.get_type()}')
+    print(f'brick.get_v_xy() = {brick.get_v_xy()}')
+    brick.move(dt=0.1)
+    print(f'brick.get_xy() = {brick.get_xy()}')
+    brick.bounce(axis='x')
+    print(f'brick.get_v_xy() = {brick.get_v_xy()}')
+    print(f'brick.hit_wall() = {brick.hit_wall()}')
+    brick.move(dt=100)
+    print(f'brick.get_xy() = {brick.get_xy()}')
+    print(f'brick.hit_wall() = {brick.hit_wall()}')
+    brick.bounce(axis='y')
+    print(f'brick.get_v_xy() = {brick.get_v_xy()}')
